cea49f7603508a6d240d73fb6cd495218ce9f3ff
Michele Capichioni <capimichi@gmail.com>
2024-11-24 23:07:16 +0100

    Added Dashboard layout component with Sidebar and Header, using a flexbox layout and tailwind classes for responsive design.

---
+++ src/layouts/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex h-screen overflow-hidden">
+    <Sidebar/>
+    <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <Header/>
+      <main>
+        <slot/>
+      </main>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Sidebar from "~/components/Dashboard/Sidebar.vue";
+import Header from "~/components/Dashboard/Header.vue";
+</script>
