4f613d93e63fccb1b75e1c8775aaf2fffb2bde13
Michele Capichioni <capimichi@gmail.com>
2024-11-24 23:06:44 +0100

    Added srcDir and configured @nuxtjs/tailwindcss module with tailwindcss options.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    srcDir: 'src/',
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    tailwindcss: {
+        exposeConfig: true,
+        viewer: true,
+        // and more...
+    }
 })
